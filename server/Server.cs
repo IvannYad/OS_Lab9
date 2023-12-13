3c3e921fadf68e376986ebffab4b4ec85c6e0cfe
IlliaShestak <illia.shestak.wk@gmail.com>
2023-12-13 19:54:31 +0200

    feat: add WeatherForecastReader

---
+++ server/Server.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server;
+using server.Data;
+using server.Models;
+using server.StaticData;
+using System.IO.Pipes;
+using System.Net.NetworkInformation;
+using System.Text.Json;
+
+namespace ServerSide {
+    public class Server {
+        private static ApplicationDbContext _context = new ApplicationDbContext();
+        private static WeatherForecastReader _weatherForecastReader;
+        private static WeatherForecastWriter _weatherForecastWriter;
+        public static async Task StartServer() {
+            var t = Task.Factory.StartNew(async () => {
+                _weatherForecastWriter = new WeatherForecastWriter();
+                _weatherForecastReader = new WeatherForecastReader();
+                while (true) {
+                    //await _weatherForecastWriter.Run();
+                    _weatherForecastReader.Run();
+                    await Task.Delay(1000);
+                }
+
+            });
+
+            var res = await t;
+            await res;
+        }
+
+        public static void StopServer() {
+            _weatherForecastReader.PipeDisconnect();
+        }
+    }
+}
---
+++ server/WeatherForecastReader.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Data;
+using server.Models;
+using server.Data;
+using server.Models;
+using server.StaticData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static server.StaticData.SD;
+
+namespace ServerSide {
+    internal class WeatherForecastReader {
+        private ApplicationDbContext _context;
+        private NamedPipeServerStream _pipe;
+        private StreamWriter _writer;
+        public WeatherForecastReader() {
+            _context = new ApplicationDbContext();
+            _pipe = new NamedPipeServerStream("WeatherPipe");
+            _writer = new StreamWriter(_pipe);
+
+        }
+
+        public void Run() {
+            try {
+                if (!_pipe.IsConnected)
+                    _pipe.WaitForConnection();
+
+                var weather = GetWeatherFromDB();
+                var line = JsonSerializer.Serialize<Weather>(weather);
+                _writer.WriteLine(line);
+                _writer.Flush();
+
+            } catch {
+                _pipe.Disconnect();
+                _pipe.Dispose();
+                _pipe = new NamedPipeServerStream("WeatherPipe");
+                _writer = new StreamWriter(_pipe);
+            }
+
+        }
+
+        private Weather GetWeatherFromDB() {
+            var weather = _context.Weathers.AsNoTracking().AsEnumerable().MaxBy(w => w.Id)!;
+            return weather;
+        }
+
+        public void PipeDisconnect() {
+            if (_pipe.IsConnected)
+                _pipe.Disconnect();
+        }
+    }
+}
